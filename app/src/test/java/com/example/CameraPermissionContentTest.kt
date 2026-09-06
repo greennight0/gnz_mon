644fe99a11fd644d08580e8902b1f24dc72a6134
greennight0 <greennight0@protonmail.com>
2026-09-06 15:30:30 +0700

    Gate scanner HUD on camera permission

---
+++ app/src/test/java/com/example/CameraPermissionContentTest.kt
@@ -0,0 +1,52 @@
+package com.example
+
+import androidx.compose.ui.test.assertDoesNotExist
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import com.example.data.model.AppLanguage
+import com.example.ui.components.ScannerOverlay
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [36])
+class CameraPermissionContentTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun deniedPermission_requestButtonIsNotBlockedByScannerOverlay() {
+        var permissionRequestCount = 0
+        var targetTapCount = 0
+
+        composeTestRule.setContent {
+            CameraPermissionContent(
+                isCameraPermissionGranted = false,
+                language = AppLanguage.ENGLISH,
+                onRequestPermission = { permissionRequestCount++ },
+                onTryDemo = {}
+            ) {
+                ScannerOverlay(
+                    detectedSpecies = null,
+                    isAnalyzing = false,
+                    language = AppLanguage.ENGLISH,
+                    onSpeciesClick = {},
+                    onCaptureClick = {},
+                    onTapCreateOrMoveTarget = { _, _ -> targetTapCount++ }
+                )
+            }
+        }
+
+        composeTestRule.onNodeWithTag("scanner_overlay_container").assertDoesNotExist()
+        composeTestRule.onNodeWithTag("request_camera_permission_button").performClick()
+
+        assertEquals(1, permissionRequestCount)
+        assertEquals(0, targetTapCount)
+    }
+}
